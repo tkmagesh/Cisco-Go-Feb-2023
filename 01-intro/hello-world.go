ae2f227b9e682f01921315c0b680948fe6ec972c
Magesh Kuppan <tkmagesh77@gmail.com>
2023-02-08 11:30:33 +0530

    hello world & cross compilation

---
+++ 01-intro/hello-world.go
@@ -0,0 +1,20 @@
+/* package declaration (mandatory) */
+package main
+
+/* import dependency packages */
+import "fmt"
+
+/* package scope types & variable declarations */
+
+/* package init function */
+
+/* main function */
+func main() {
+	var userName string = "Magesh"
+	// print("Hello World!")
+	// println("Hello World!")
+	// println("Hi", userName, "Have a nice day!")
+	fmt.Printf("Hi %s, Have a nice day!\n", userName)
+}
+
+/* other functions */
