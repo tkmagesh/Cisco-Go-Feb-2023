7e63875fff1e6eab3c615fac712d6196a447bf99
Magesh Kuppan <tkmagesh77@gmail.com>
2023-02-09 10:37:04 +0530

    updates

--- 07-panic-recovery/program.go
+++ 07-panic-recovery/program.go
@@ -10,24 +10,28 @@ var DivideByZeroError = errors.New("divisor cannot be 0")
 
 func main() {
 	defer func() {
-		if e := recover(); e != nil {
-			log.Println("panicked..", e)
-			if err, ok := e.(error); ok {
-				if errors.Is(err, DivideByZeroError) {
-					fmt.Println("Do not divide by 0")
-				}
+		if err, ok := recover().(error); ok {
+			log.Println("panicked..", err)
+			if errors.Is(err, DivideByZeroError) {
+				fmt.Println("Do not divide by 0")
 			}
-
 		}
 		fmt.Println("Thank you")
 	}()
+
 	divisor := 0
-	q, r, err := divideClient(100, divisor)
-	if err == DivideByZeroError {
-		fmt.Println("Do not attempt to divide by 0")
-		return
-	}
+	q, r := divide(100, divisor)
 	fmt.Printf("Dividing 100 by %d, quotient = %d and remainder = %d\n", divisor, q, r)
+
+	//converting a panic into an error (using the "divideClient" function)
+	/*
+		q, r, err := divideClient(100, divisor)
+		if err == DivideByZeroError {
+			fmt.Println("Do not attempt to divide by 0")
+			return
+		}
+		fmt.Printf("Dividing 100 by %d, quotient = %d and remainder = %d\n", divisor, q, r)
+	*/
 }
 
 func divideClient(x, y int) (quotient, remainder int, err error) {
