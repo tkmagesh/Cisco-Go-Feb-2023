dbe9fdee08a9a3fdf028b56c8cfa55fc296d3ec0
Magesh Kuppan <tkmagesh77@gmail.com>
2023-02-10 12:30:52 +0530

    CSM vs SMC

---
+++ 15-concurrency/10-demo.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	//share memory by communicating
+	/*
+		var ch chan int
+		ch = make(chan int)
+	*/
+	ch := make(chan int)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go add(100, 200, wg, ch)
+	wg.Wait()
+	result := <-ch
+	fmt.Println(result)
+}
+
+func add(x, y int, wg *sync.WaitGroup, ch chan int) {
+	defer wg.Done()
+	result := x + y
+	ch <- result
+}
